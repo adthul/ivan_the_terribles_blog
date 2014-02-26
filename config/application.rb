30f51a6836f42ea47a8afd529534ed9e326cad48
Andrew Thul <thul.andy@gmail.com>
2014-02-25 18:25:26 -0800

    add heroku crap

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module IvanTheTerriblesBlog
       g.test_framework :rspec
     end
 
+    config.assets.initialize_on_precompile = false
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
